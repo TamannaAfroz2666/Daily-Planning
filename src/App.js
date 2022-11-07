b0f4608147cf3050d2357e6841b1cb108d0efdef
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-07 14:42:48 +0600

    add link and navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router , Route, Link} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { Input  } from 'semantic-ui-react'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         welcome
-        </p>
-       
-      </header>
-    </div>
+
+    <Router> 
+    
+      <div className="container fluid ">
+        <header className="App-header1">
+          <p className='para h1 pt-5'> Welcome </p>
+            <div className='mt-5'>
+              <div class="ui secondary  menu">
+              <Link to='/home' className='item'>
+              Home 
+              </Link>
+              <Link to='/about' className='item'>
+              About 
+              </Link>
+              <Link to='/products' className='item'>
+              Product
+              </Link>                
+                <div class="right menu">
+                  <div class="item">
+                    <div class="ui icon input">
+                      <Input type="text" placeholder="Search..." />
+                      <i class="search link icon"></i>
+                </div>
+            </div>
+            <a class="ui item">
+              Logout
+            </a>
+          </div>
+        </div>
+          </div>
+          
+          
+        {/* <div className='from'>
+        <Form className='from-child'>
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder='First Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input placeholder='Last Name' />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox label='I agree to the Terms and Conditions' />
+            </Form.Field>
+            <Button type='submit' className='btn-ui-class'>Submit</Button>
+         </Form>
+         
+        </div> */}
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/about' component={About}/>
+          <Route exact path='/products' component={Products}/>
+        </header>
+      </div>
+   
+    </Router> 
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <div>
         <h1> Home Page </h1>
+        <h2>welcome to new home </h2>
+        <div>
+
+          
+
+        </div>
     </div>
   )
 }

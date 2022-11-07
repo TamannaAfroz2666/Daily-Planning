ff45901df86fc9c9b17799ee4bf00ae12813aacb
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-07 16:43:25 +0600

    add modal

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import  './About.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 const About = () => {
 
     const bookingHandle =() =>{
-        console.warn("yyyy");
+        console.warn("Are you ready to book our Air tickets");
+        setShow(true)
     }
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    
   return (
    <>
      <div className='parent-class'> 
@@ -21,6 +27,25 @@ const About = () => {
                 <h3 className='title'>Book online and begin your dream holyday</h3>
                 <p className='description'>Travel with the Airline of the Year </p>
                 <Button variant="primary" className='picture-button'  onClick={bookingHandle}>Book now</Button>{' '}
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                <Modal.Header closeButton>
+                <Modal.Title>Yaahoo Big Offer</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    It's 20% of off tickets price to all of our Economic Class. Book to  your ticket. Happy Journey......
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
             </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -34,6 +59,25 @@ const About = () => {
                 <h3 className='title'>Book online and begin your dream holyday</h3>
                 <p className='description'>Travel with the Airline of the Year</p>
                 <Button variant="primary" className='picture-button'  onClick={bookingHandle}>Book now</Button>{' '}
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                <Modal.Header closeButton>
+                <Modal.Title>Yaahoo Big Offer</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    It's 20% of off tickets price to all of our Economic Class. Book to  your ticket. Happy Journey......
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
             </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -47,6 +91,25 @@ const About = () => {
                 <h3 className='title'>Book online and begin your dream holyday</h3>
                 <p className='description'> Travel with the Airline of the Year</p>
                 <Button variant="primary" className='picture-button' onClick={bookingHandle}>Book now</Button>{' '}
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                <Modal.Header closeButton>
+                <Modal.Title>Yaahoo Big Offer</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    It's 20% of off tickets price to all of our Economic Class. Book to  your ticket. Happy Journey......
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>    
             </Carousel.Caption>
             </Carousel.Item>
         </Carousel>

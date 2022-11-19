39c4d8ab46a0c9b940d4569a534168f10df81466
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-19 11:30:27 +0600

    add desing in product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import './Products.css'
+import { Button, Form } from 'semantic-ui-react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+
+      <div className='container1'>
+        
+
+        <div className="main shadow-lg rounded">
+        <h1 className='header-title text-center mt-5 '>Product List</h1>
+          <Form>
+              <Form.Field>
+                <label>Product Name</label>
+                <input type='text' placeholder='name' />
+              </Form.Field>
+              <Form.Field>
+                <label>Product Size</label>
+                <input type='number' placeholder='size' />
+              </Form.Field>
+              <Form.Field>
+                <label>Product Price</label>
+                <input placeholder='price' />
+              </Form.Field>
+              <Form.Field>
+                <label>Product Quantity</label>
+                <input placeholder='quantity' />
+              </Form.Field>
+              <Button type='submit'>Add Cart</Button>
+          </Form>
+        </div>
+
+
+      </div>
+
+
+    </div>
   )
 }
 

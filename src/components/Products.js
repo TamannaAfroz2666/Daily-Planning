11f4d02894437e2b4f1a57bf1d299c2409521306
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-20 16:06:54 +0600

    add table header

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Products.css'
 import { Button, Form } from 'semantic-ui-react'
+import Table from 'react-bootstrap/Table';
 
 const Products = () => {
   const [name, setName] = useState('');
@@ -24,7 +25,7 @@ const Products = () => {
   return (
     <div>
 
-      <div className='container1'>
+      <div className='container1 d-flex'>
         
 
         <div className="main shadow-lg rounded">
@@ -49,7 +50,42 @@ const Products = () => {
               <Button className='card-button mt-4' type='submit' onClick={cardButton}>Add Cart</Button>
           </Form>
         </div>
+        <div className='tableClass '>
 
+
+          <Table striped bordered hover>
+            <thead>
+              <tr className='table-row'>
+                <th >Artical Number</th>
+                <th>Product Name</th>
+                <th>Product Size</th>
+                <th>Product Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {/* <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody> */}
+          </Table>
+
+        </div>
         
 
 
